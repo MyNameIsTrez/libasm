70743a9aa8e669bf660b17c2ae2888b62f31d96c
MyNameIsTrez <sbos@student.codam.nl>
2023-04-28 21:44:48 +0200

    Remove comment

--- asm/ft_strcmp.asm
+++ asm/ft_strcmp.asm
@@ -3,8 +3,6 @@ global _ft_strcmp
 _ft_strcmp:
 	mov rax, 0				; i = 0;
 _loop:
-	; // TODO: Return if str1[i] *and* str2[i] are '\0' *at the same time*
-
 	mov rcx, [rdi+rax]		; chr = str1[i];
 	cmp rcx, [rsi+rax]
 	jl _return_less			; if (chr < str2[i]) goto _return_less;
