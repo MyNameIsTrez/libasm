53208dc77cf314ecab2795b06e6dc92c429b6ace
MyNameIsTrez <sbos@student.codam.nl>
2023-05-02 15:35:15 +0200

    Simplify ft_strlen

--- asm/ft_strlen.asm
+++ asm/ft_strlen.asm
@@ -1,11 +1,9 @@
 global _ft_strlen
 
 _ft_strlen:
-	mov rax, 0				; i = 0;
+	mov rax, -1				; i = -1;
 _loop:
-	cmp [rdi+rax], BYTE 0
-	je _return				; if (str[i] == '\0') goto _return;
 	inc	rax					; i++;
-	jmp _loop				; goto _loop;
-_return:
+	cmp [rdi+rax], BYTE 0
+	jne _loop				; if (str[i] != '\0') goto _loop;
 	ret						; return i;
